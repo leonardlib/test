9741b0e8dadfbe1e1505c3776994b757824627d1
Leonardo Lira Becerra <leonardolirabecerra@Leonardos-MacBook-Pro-2.local>
2018-03-11 15:20:46 -0600

    Get message data

--- index.js
+++ index.js
@@ -1,26 +1,55 @@
 //Dependencies imports
 var express = require('express');
-var body_parser = require('body-parser');
+var bodyParser = require('body-parser');
 var request = require('request');
 var env = require('node-env-file');
 
 env('.env');
 var app = express();
-app.use(body_parser.json());
+app.use(bodyParser.json());
 
 //Listener port
 app.listen(8000, function () {
     console.log('Server is active on port 8000');
 });
 
-//Routes
+/*------------------------------------------------ Routes ------------------------------------------------------------*/
 app.get('/', function (req, res) {
     res.send('Welcome!');
 });
+
+//Server validation
 app.get('/webhook', function (req, res) {
-    if (req.query['hub.verify_token'] === process.env.WEBHOOK_TOKEN) {
+    if (req.query['hub.verify_token'] == process.env.WEBHOOK_TOKEN) {
         res.send(req.query['hub.challenge']);
     } else {
         res.send("You don't have access!");
     }
 });
+
+//Get message request data
+app.post('/webhook', function (req, res) {
+    var data = req.body;
+
+    if (data.object == 'page') {
+        data.entry.forEach(function (entry) {
+            entry.messaging.forEach(function (messageEvent) {
+                if (messageEvent.message) {
+                    getMessageData(messageEvent);
+                }
+            });
+        });
+
+        res.sendStatus(200);
+    }
+});
+
+/*-------------------------------------------- Functions -------------------------------------------------------------*/
+/**
+ * Function to get data from a message
+ * @param event
+ */
+function getMessageData(event) {
+    var senderId = event.sender.id;
+    var messageText = event.message.text;
+}
